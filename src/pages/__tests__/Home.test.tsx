bacf3213c3d0adf143b12ca1ca716e0ed9429a23
kimdobie <kimdobie@gmail.com>
2023-08-13 21:06:07 -0500

    Fix build issues

--- src/pages/__tests__/Home.test.tsx
+++ src/pages/__tests__/Home.test.tsx
@@ -1,12 +1,17 @@
 /* eslint-disable react/react-in-jsx-scope */
 import { axe } from 'jest-axe';
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router';
 
 import Home from '../../pages/Home';
 
 describe('Home component tests', () => {
   test('Home component is accessible', async () => {
-    const { container } = render(<Home />);
+    const { container } = render(
+      <MemoryRouter>
+        <Home />
+      </MemoryRouter>
+    );
     const results = await axe(container);
     expect(results).toHaveNoViolations();
   });
